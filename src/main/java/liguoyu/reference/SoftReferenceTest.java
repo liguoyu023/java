4f5b802e9af52c04405666b422bca5981471d268
liguoyu023 <821566613@qq.com>
2016-07-11 16:07:13 +0800

    update

---
+++ src/main/java/liguoyu/reference/SoftReferenceTest.java
@@ -0,0 +1,22 @@
+package liguoyu.reference;
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+
+/**
+ * Created by liguoyu@58.com on 2016/7/11.
+ */
+public class SoftReferenceTest {
+    public static void main(String[] args) {
+        Object object = new Object();
+        ReferenceQueue queue = new ReferenceQueue();
+        SoftReference aSoftReference = new SoftReference(object,queue);
+
+        SoftReference ref = null;
+        while ((ref = (SoftReference) queue.poll()) != null) {
+            // 清除ref
+            ref = null;
+        }
+
+    }
+}  
